a6ff10678d48521207861fd58e6e2fba8721ad46
alex <alex@pdp7.net>
2021-02-21 13:50:14 +0100

    WIP: Use ToString in main

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ use plankalkul::expr_parse;
 use std::env::args;
 
 fn main() {
-    println!("{:?}", expr_parse::expr(args().nth(1).unwrap().as_str()));
+    let arg = args().nth(1).unwrap();
+    let (rest, expr) = expr_parse::expr(arg.as_str()).unwrap();
+    assert_eq!("", rest);
+    println!("{}", expr.to_string());
 }
