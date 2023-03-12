468a821ebe21f45458710d3c985fd82003c8e7de
Alex Corcoles <alex@pdp7.net>
2023-03-12 20:46:32 +0100

    Update yew (#50)
    * Apply fixes for yew 0.20.0
    * Run cargo update
    * Bump yew from 0.19.3 to 0.20.0
    
    Bumps [yew](https://github.com/yewstack/yew) from 0.19.3 to 0.20.0.
    - [Release notes](https://github.com/yewstack/yew/releases)
    - [Changelog](https://github.com/yewstack/yew/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/yewstack/yew/compare/yew-v0.19.3...yew-v0.20.0)
    
    ---
    updated-dependencies:
    - dependency-name: yew
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- plankalkul-pwa/src/app.rs
+++ plankalkul-pwa/src/app.rs
@@ -25,7 +25,7 @@ impl Component for Model {
         }
     }
 
-    fn changed(&mut self, _ctx: &Context<Self>) -> bool {
+    fn changed(&mut self, _ctx: &Context<Self>, _old_props: &Self::Properties) -> bool {
         false
     }
 
